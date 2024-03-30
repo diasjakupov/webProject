f44b5e83b08f74b321aa7d98e1d5b05016526771
szagyarorszag <bakitzhana566@gmail.com>
2024-03-30 20:39:36 +0500

    checking home component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, Component } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 import { routes } from './app.routes';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule /* другие модули */],
+  templateUrl: './pages/home/home.component.html',
+  styleUrls: ['./pages/home/home.component.css']
+})
+export class HomeComponent {
+  // Логика компонента...
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
