fc3fee31384854f790a7207a4b11583f1d413b2c
Makiell <damir.sharipov99@mail.ru>
2024-04-20 17:57:26 +0600

    Like serializer+view

--- backend/social/urls.py
+++ backend/social/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView, CommentListView, LikeListView
+from .views import *
 
 urlpatterns = [
     path("posts/", PostListView.as_view()),
     path("comments/<int:pk>", CommentListView.as_view()),
-    path("likes/<int:pk>", LikeListView.as_view())
+    path("likes/<int:pk>", LikeListView.as_view()),
+    path("like", like_post)
 ]
--- backend/social/views.py
+++ backend/social/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 from .models import Post, Comment, Like
+from user.models import *
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 
@@ -23,3 +28,28 @@ class LikeListView(generics.ListAPIView):
 
     def get_queryset(self):
         return Like.objects.filter(post__pk = self.kwargs.get("pk"))
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def like_post(request):
+
+    user = request.user
+
+    post_id = request.data.get('post_id')
+
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        return Response({'error': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    like = Like.objects.filter(user=user, post=post).first()
+    if like:
+        like.delete()
+        return Response({'message': 'Like already exists.'}, status=status.HTTP_409_CONFLICT)
+
+
+    serializer = LikeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
